74738280dfbdd31ffe1d8c02e2837921306a1bf0
Drina 3 <musiliidrina@gmail.com>
2025-12-11 05:17:44 +0300

    Remove Streamlit installation command
    Removed the Streamlit installation command from the script.

--- wine_quality_prediction.py
+++ wine_quality_prediction.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/18u_f7mKuijtLkM2GOiAjlfavDnNqSkTi
 """
 
-!pip install streamlit
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
